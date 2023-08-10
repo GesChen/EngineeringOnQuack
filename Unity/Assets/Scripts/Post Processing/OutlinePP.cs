2ee21791b1cdef7ccd04536bab600dc3edb3d5c6
GesChen <omgtntlol101@gmail.com>
2023-08-10 13:08:59 -0400

    basic outline shader
    need to make it apply to only one obj tho somehow idkk

---
+++ Unity/Assets/Scripts/Post Processing/OutlinePP.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/OutlinePP")]
+public sealed class OutlinePP : CustomPostProcessVolumeComponent, IPostProcessComponent
+{
+    [Tooltip("Controls the intensity of the effect.")]
+    public ClampedFloatParameter intensity = new ClampedFloatParameter(0f, 0f, 1f);
+    public FloatParameter thickness = new FloatParameter(1f);
+    public ColorParameter color = new ColorParameter(Color.black);
+    public FloatParameter power = new FloatParameter(1f);
+    public FloatParameter minDepth = new FloatParameter(0f);
+    public FloatParameter maxDepth = new FloatParameter(1f);
+
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null && intensity.value > 0f;
+
+    // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > Graphics > HDRP Global Settings).
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    const string kShaderName = "Hidden/Shader/OutlinePP";
+
+    public override void Setup()
+    {
+        if (Shader.Find(kShaderName) != null)
+            m_Material = new Material(Shader.Find(kShaderName));
+        else
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume OutlinePP is unable to load.");
+    }
+
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+    {
+        if (m_Material == null)
+            return;
+
+        m_Material.SetFloat("_Intensity", intensity.value);
+        m_Material.SetFloat("_Thickness", thickness.value);
+        m_Material.SetColor("_Color",     color.value);
+        m_Material.SetFloat("_Power",     power.value);
+        m_Material.SetFloat("_MinDepth",  minDepth.value);
+        m_Material.SetFloat("_MaxDepth",  maxDepth.value);
+
+        m_Material.SetTexture("_MainTex", source);
+        HDUtils.DrawFullScreen(cmd, m_Material, destination, shaderPassId: 0);
+    }
+
+    public override void Cleanup()
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+}
