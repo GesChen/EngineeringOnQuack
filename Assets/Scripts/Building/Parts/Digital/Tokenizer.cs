02b57faede79301ebbb3ade6bd3bd9073b0cbd38
GesChen <gesch3n@gmail.com>
2025-03-03 12:14:24 -0800

    tokenizer start

--- Assets/Scripts/Building/Parts/Digital/Tokenizer.cs
+++ Assets/Scripts/Building/Parts/Digital/Tokenizer.cs
@@ -6,8 +6,6 @@ using System.Text;
 public class Tokenizer {
 	public string RemoveComments(string lines) {
 		int opstate = 0; // 0-looking for comment, 1-in oneline comment, 2-in multiline comment
-		int searchstate = 0; // 0-looking for more -s 
-
 		int i = 0;
 		StringBuilder sb = new();
 
@@ -50,9 +48,58 @@ public class Tokenizer {
 		return sb.ToString();
 	}
 
+	public List<Token> TokenizeLine(string line)
+	{
+		return new();
+	}
+
+	public Section SplitSection(string[] lines, int startLineNum)
+	{
+		List<Line> sectionLines = new();
+		
+		int indentation(int linenum)
+		{
+			string line = lines[linenum];
+			int n = 0;
+			int indentation = 0;
+			while (char.IsWhiteSpace(line[n]))
+			{
+				if (line[n] == ' ') indentation += 1;
+				else if (line[n] == '\t') indentation += LanguageConfig.SpacesPerTab;
+				n++;
+			}
+			return indentation;
+		}
+
+		int startIndentation = indentation(0);
+		int i = 0;
+		while (i < lines.Length)
+		{
+			if (indentation(i) > startIndentation)
+			{
+				List<string> subSecStrings = new();
+				int n = i; // get entire chunk of indented text
+				while (n < lines.Length && indentation(n) > startIndentation)
+				{
+					subSecStrings.Add(lines[n]);
+					n++;
+				}
+
+				sectionLines.Add(new(startLineNum + i, lines[i], // add a subsection
+                    SplitSection(subSecStrings.ToArray(), n))); // recurse deeper sections, give starting index
+			}
+			else
+			{
+				sectionLines.Add(new(startLineNum + i, lines[i], TokenizeLine(lines[i])));
+			}
+		}
+		return new(sectionLines);
+	}
+
 	public Section Tokenize(string lines) {
 		lines = RemoveComments(lines);
-		
-		return null;
+
+		return SplitSection(lines.Split('\n'), 0);
 	}
+	
 }
